c8dfa6557ae1d1ae95f4d18f8315e433eca43c7b
王华伟 <361385654@qq.com>
2021-08-18 14:09:59 +0800

    选择指定plist文件

--- textureUnpacker/panel/index.js
+++ textureUnpacker/panel/index.js
@@ -31,71 +31,136 @@ Editor.Panel.extend({
     $: {
         selectDiv: "#select",
         nowPathDiv: "#now",
-        plistTexture: "#ptexture",
+        plistTexture1: "#ptexture1",
+        plistTexture2: "#ptexture2",
         choice1: "#choice1",
         choice2: "#choice2",
         plistDiv: "#plistDiv",
         exportBtn1: "#exportBtn1",
         exportBtn2: "#exportBtn2",
+        selectFrame: "#selectFrame",
+        tips: "#tips",
     },
 
     ready() {
         this.addListener();
-
-
     },
 
     addListener() {
         this.$choice1.onclick = this.onChangeRes.bind(this, 1);
         this.$choice2.onclick = this.onChangeRes.bind(this, 2);
 
+        this.$exportBtn1.onclick = this.exportCall.bind(this);
+        this.$exportBtn2.onclick = this.exportCall.bind(this);
+
         this.$plistDiv.onchange = this.onChangePlist.bind(this);
 
-        this.$exportBtn1.onclick = this.exportCall1.bind(this);
-        this.$exportBtn2.onclick = this.exportCall2.bind(this);
+        this.$selectFrame.ondragenter = this.dragenter.bind(this);
+        this.$selectFrame.ondragover = this.dragover.bind(this);
+        this.$selectFrame.ondrop = this.drop.bind(this);
+        this.$selectFrame.onclick = this.selectPlist.bind(this);
     },
+
+    dragenter(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    },
+    dragover(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    },
+    drop(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let dt = event.dataTransfer;
+        let file = dt.files;
+        if (file[0]) {
+            let blob = file[0];
+            // let url = URL.createObjectURL(blob)
+            // Editor.log(url)
+            var reader = new FileReader();
+            Editor.log(reader)
+            reader.onload = (() => {
+                Editor.log(reader.result)
+            })
+            reader.readAsDataURL(blob);
+        }
+    },
+
+    selectPlist() {
+        let plistpath = this.selectFile();
+        if (plistpath) {
+            this.$tips.style = "display: none;";
+            this.$exportBtn2.style = "visibility: visible;";
+            this.showPng(String(plistpath), this.$plistTexture2);
+        }
+    },
+
+    showPng(plistpath, div) {
+        let pngpath = plistpath.substring(0, plistpath.lastIndexOf(".")) + ".png"
+
+        let flag = Fs.existsSync(pngpath);
+        if (flag) {
+            div.innerHTML = `<div>
+                    <img src="${pngpath}" alt="图片文件不存在" board="3px" ></img>
+                </div>`;
+            this.selectPlistPath = plistpath;
+        } else {
+            Editor.error("png文件不存在！-", pngpath)
+        }
+    },
+
 
     /** 选择其他plist文件 */
     onChangePlist() {
         let index = this.$plistDiv.selectedIndex;
-        this.$plistTexture.style = index == 0 ? "display:none;" : "display:block;";
+        this.$plistTexture1.style = index == 0 ? "display:none;" : "display:block;";
         this.$exportBtn1.style = index == 0 ? "visibility: hidden;" : "visibility: visible;";
         this.selectPlistPath = null;
 
         if (index == 0) return;
 
-        let plist = plistFile[index - 1];
-        plist = plist.replace(/\//g, "\\");
-        let pngpath = path.dirname(plist) + `\\${this.$plistDiv.options[index].value}.png`;
-
-        this.$plistTexture.innerHTML = `<div>
-            <img id="plistImg" src="${pngpath}" alt="loading..." board="3px" ></img>
-        </div>`;
-
-        this.selectPlistPath = plist;
+        let plistpath = plistFile[index - 1];
+        plistpath = plistpath.replace(/\//g, "\\");
+        this.showPng(plistpath, this.$plistTexture1);
     },
 
     onChangeRes(id) {
+        this.selectPlistPath = null;
+
+        this.$exportBtn1.style = "visibility: hidden;";
+        this.$exportBtn2.style = "visibility: hidden;";
+
+        this.$tips.style = "display: block;";
+
+        this.$plistTexture1.innerHTML = ``;
+        this.$plistTexture2.innerHTML = ``;
+
         this.$selectDiv.style = id == 1 ? "display:none;" : "display:block;"
         this.$nowPathDiv.style = id == 2 ? "display:none;" : "display:block;"
     },
 
-    exportCall1() {
+    exportCall() {
         if (!this.selectPlistPath) return;
-        this.selectFolder((dirpath) => {
-            PlistTool.unpack(this.selectPlistPath, dirpath);
+        let dirpath = this.selectFolder();
+        PlistTool.unpack(this.selectPlistPath, dirpath);
+    },
+
+    selectFile() {
+        let filePath = dialog.showOpenDialogSync({
+            filters: [{
+                name: 'plist',
+                extensions: ['plist']
+            }],
+            properties: ['openFile', ],
         })
-    },
-    exportCall2() {
-
+        return filePath;
     },
 
-    selectFolder(func) {
-        //showSaveDialog(): Promise
-        dialog.showSaveDialog({
+    selectFolder() {
+        let filePath = dialog.showSaveDialogSync({
             properties: ['openDirectory', ]
-        }).then((result) => {
-            func(result.filePath);
-        });
+        })
+        return filePath;
     },
 });
